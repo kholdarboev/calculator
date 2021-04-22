172fdd0c8485c4b4c2b169a277d9a1d3dcf39a80
sardorbek-sf <sardor4ik2013@gmail.com>
2021-04-22 00:38:43 -0700

    first calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Button from './button'
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends Component {
+constructor(props){
+  super(props)
+  this.state = {
+    current: '',
+    previous: [],
+    Reset : false,
+  }
 }
 
-export default App;
+reset = () => {
+  this.setState({current: '0', previous:[]});
+}
+
+addToCurrent =(symbol) => {
+  if (['*','/','-','+'].indexOf(symbol)> -1){
+    let {previous} = this.state;
+    previous.push(this.state.current + symbol);
+    this.setState({previous, Reset : true});
+  }else{
+    if((this.state.current === "0" && symbol !== ".") || (this.state.Reset)){
+      this.setState({current: symbol, Reset:false})
+    }else{
+      this.setState({current:this.state.current + symbol});
+      
+    }
+  }
+}
+calculate = (symbol) => {
+  let {current, previous, Reset} = this.state;
+ if (previous.length > 0){
+
+   current = eval(String(previous[previous.length -1] + current));
+   this.setState({current, previous:[], Reset: true}); 
+ }
+
+}
+
+
+render() {
+
+const buttons = [
+    {symbol: 'C', cols: 3, action:this.reset },
+    {symbol: '/', cols:1, action: this.addToCurrent},
+    {symbol: '7', cols:1, action: this.addToCurrent},
+    {symbol: '8', cols:1, action: this.addToCurrent},
+    {symbol: '9', cols:1, action: this.addToCurrent},
+    {symbol: '*', cols:1, action: this.addToCurrent},
+    {symbol: '4', cols:1, action: this.addToCurrent},
+    {symbol: '5', cols:1, action: this.addToCurrent},
+    {symbol: '6', cols:1, action: this.addToCurrent},
+    {symbol: '-', cols:1, action: this.addToCurrent},
+    {symbol: '1', cols:1, action: this.addToCurrent},
+    {symbol: '2', cols:1, action: this.addToCurrent},
+    {symbol: '3', cols:1, action: this.addToCurrent},
+    {symbol: '+', cols:1, action: this.addToCurrent},
+    {symbol: '0', cols:2, action: this.addToCurrent},
+    {symbol: '.', cols:1, action: this.addToCurrent},
+    {symbol: '=', cols:1, action: this.calculate},
+];
+
+
+    return (
+      <div className='container'>
+        {this.state.previous.length > 0 ?
+        <div className="last">{this.state.previous[this.state.previous.length -1]}</div>  
+      :null  
+      }
+
+
+
+        <input type="text" className='result' 
+        value={this.state.current}/>
+
+
+        {buttons.map((btn,index)=>{
+          return <Button key={index} symbol={btn.symbol} 
+          cols={btn.cols} action={(symbol)=> btn.action(symbol)} />
+        })}
+
+      </div>
+    )
+  }
+}
+
+export default App
